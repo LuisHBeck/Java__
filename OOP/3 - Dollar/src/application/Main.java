26f8ab98be3320ebb9c5d010e44c8c99cdd48e3e
LuisHBeck <beck.henrique.luis@gmail.com>
2023-09-29 11:26:52 -0300

    feat: static members

---
+++ OOP/3 - Dollar/src/application/Main.java
@@ -0,0 +1,23 @@
+package application;
+
+import entities.CurrencyConverter;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double price, quantity;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("What is the dollar price? ");
+        price = sc.nextDouble();
+        System.out.println("");
+
+        System.out.print("How many dollars will be bought? ");
+        quantity = sc.nextDouble();
+        System.out.println("");
+        sc.close();
+
+        System.out.printf("Amount to be paid id reais = R$%f", CurrencyConverter.dollarToReal(price, quantity));
+    }
+}
---
+++ OOP/3 - Dollar/src/entities/CurrencyConverter.java
@@ -0,0 +1,9 @@
+package entities;
+
+public class CurrencyConverter {
+
+    public static final double IOF = 0.06;
+    public static double dollarToReal(double price, double quantity) {
+        return (price*quantity)*IOF;
+    }
+}
